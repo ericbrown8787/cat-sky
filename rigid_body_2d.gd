7d058ae50733952025f91183b7119c10f617fd3a
Eric Brown <eric.brown8787@gmail.com>
2025-01-25 11:19:06 -0500

    Add background draft and bottom, right left level limits

--- rigid_body_2d.gd
+++ rigid_body_2d.gd
@@ -1,20 +1,17 @@
 extends RigidBody2D
 
-var vertical_accel = Vector2(0,20)
-var horizontal_accel = Vector2(20,0) 
+var vertical_accel = Vector2(0,5)
+var horizontal_accel = Vector2(10,0) 
 var thrust = Vector2()
 @onready var camera = $Camera2D
+var camera_size
 
 func _ready():
 	camera.make_current()
+	camera_size = get_viewport_rect().size * camera.zoom
+	print(camera_size)
 
-func get_input():
-	if Input.is_action_pressed("down"):
-		print("down")
-		
-		
-
-func _physics_process(delta):
+func handle_input():
 	if Input.is_action_pressed("up"):
 		self.apply_impulse(-vertical_accel)
 	if Input.is_action_pressed("down"):
@@ -23,4 +20,8 @@ func _physics_process(delta):
 		self.apply_impulse(-horizontal_accel)
 	if Input.is_action_pressed("right"):
 		self.apply_impulse(horizontal_accel)		
-		
+
+func _physics_process(delta):
+	handle_input()
+	
+	
